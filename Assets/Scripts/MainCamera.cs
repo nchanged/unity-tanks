fa61eaab0641244d7d0877f29ad50306700dae55
iorlov <nchanged@gmail.com>
2014-01-21 16:40:19 +0200

    Chaning to c#

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+	
+	public float dragSpeed = 1;
+    private Vector3 dragOrigin;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+		
+        if (!Input.GetMouseButton(0)) 
+			return;
+		
+		Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x, 0, pos.y);
+        transform.Translate(move, Space.World);
+    }
+}
